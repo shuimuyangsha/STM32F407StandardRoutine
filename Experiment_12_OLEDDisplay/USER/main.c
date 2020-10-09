b0c1b0b673298d68245b8d328d3f9953d6b96c42
shuimuyangsha <1023178387@qq.com>
2020-10-09 20:19:12 +0800

    ~添加工程 Experiment_12_OLEDDisplay

--- Experiment_12_OLEDDisplay/USER/main.c
+++ Experiment_12_OLEDDisplay/USER/main.c
@@ -12,6 +12,7 @@
 //广州市星翼电子科技有限公司  
 //作者：正点原子 @ALIENTEK
 
+u8 uCProbeDebug_t = 0;
 
 int main(void)
 { 
@@ -23,19 +24,20 @@ int main(void)
  	OLED_Init();				//初始化OLED
   OLED_ShowString(0,0,"ALIENTEK",24);  
 	OLED_ShowString(0,24, "0.96' OLED TEST",16);  
- 	OLED_ShowString(0,40,"ATOM 2014/5/4",12);  
+ 	OLED_ShowString(0,40,"Eden 2020/10/09",12);  
  	OLED_ShowString(0,52,"ASCII:",12);  
  	OLED_ShowString(64,52,"CODE:",12);  
 	OLED_Refresh_Gram();//更新显示到OLED	 
 	t=' ';  
 	while(1) 
-	{		
+	{	
+		uCProbeDebug_t = t;
 		OLED_ShowChar(36,52,t,12,1);//显示ASCII字符	
 		OLED_ShowNum(94,52,t,3,12);	//显示ASCII字符的码值    
 		OLED_Refresh_Gram();        //更新显示到OLED
 		t++;
 		if(t>'~')t=' ';  
-		delay_ms(500);
+		delay_ms(20);
 		LED0=!LED0;
 	}
 }
