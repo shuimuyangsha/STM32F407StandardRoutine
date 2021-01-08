fa43514d14f36ed7e51e0dca310dbcba1f4b441f
shuimuyangsha <1023178387@qq.com>
2021-01-08 19:59:35 +0800

    ~ 修改 BootLoader 按键逻辑

--- Experiment_50_Serial_IAP/IAP_Bootloader_V1_0/USER/main.c
+++ Experiment_50_Serial_IAP/IAP_Bootloader_V1_0/USER/main.c
@@ -136,26 +136,15 @@ int main(void)
 	u32 oldcount = 0;	//老的串口接收数据值
 	u32 applenth = 0;	//接收到的app代码长度
 	u8 clearflag = 0;
+	u8 first_run_flag = 1;
 
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
 	delay_init(168);  //初始化延时函数
 	uart_init(460800);		//初始化串口波特率为115200
 
 	LED_Init();					//初始化LED 
-	//LCD_Init();					//LCD初始化  
 	KEY_Init();					//按键初始化 
-	POINT_COLOR = RED;//设置字体为红色 
-	LCD_ShowString(30, 50, 200, 16, 16, "Explorer STM32F4");
-	LCD_ShowString(30, 70, 200, 16, 16, "IAP TEST");
-	LCD_ShowString(30, 90, 200, 16, 16, "ATOM@ALIENTEK");
-	LCD_ShowString(30, 110, 200, 16, 16, "2014/7/21");
-	LCD_ShowString(30, 130, 200, 16, 16, "KEY_UP:Copy APP2FLASH");
-	LCD_ShowString(30, 150, 200, 16, 16, "KEY1:Erase SRAM APP");
-	LCD_ShowString(30, 170, 200, 16, 16, "KEY0:Run SRAM APP");
-	LCD_ShowString(30, 190, 200, 16, 16, "KEY2:Run FLASH APP");
-	POINT_COLOR = BLUE;
-	//显示提示信息
-	POINT_COLOR = BLUE;//设置字体为蓝色	  
+
 	while (1)
 	{
 		if (USART_RX_CNT)
@@ -186,97 +175,48 @@ int main(void)
 		}
 
 		key = KEY_Scan(0);
-		if (key == WKUP_PRES)	//WK_UP按键按下
-		{
-			if (applenth)
-			{
-				printf("开始更新固件...\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "Copying APP2FLASH...");
-				if (((*(vu32*)(0X20001000 + 4)) & 0xFF000000) == 0x08000000)//判断是否为0X08XXXXXX.
-				{
-					iap_write_appbin(FLASH_APP1_ADDR, USART_RX_BUF, applenth);//更新FLASH代码   
-					LCD_ShowString(30, 210, 200, 16, 16, "Copy APP Successed!!");
-					printf("固件更新完成!\r\n");
-				}
-				else
-				{
-					LCD_ShowString(30, 210, 200, 16, 16, "Illegal FLASH APP!  ");
-					printf("非FLASH应用程序!\r\n");
-				}
-			}
-			else
-			{
-				printf("没有可以更新的固件!\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "No APP!");
-			}
-			clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示									 
-		}
-		if (key == KEY1_PRES)	//KEY1按下
-		{
-			if (applenth)
-			{
-				printf("固件清除完成!\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "APP Erase Successed!");
-				applenth = 0;
-			}
-			else
-			{
-				printf("没有可以清除的固件!\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "No APP!");
-			}
-			clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示									 
-		}
-		//if (key == KEY2_PRES)	//KEY2按下
-		//{
-		//	printf("开始执行FLASH用户代码!!\r\n");
-		//	if (((*(vu32*)(FLASH_APP1_ADDR + 4)) & 0xFF000000) == 0x08000000)//判断是否为0X08XXXXXX.
-		//	{
-		//		iap_load_app(FLASH_APP1_ADDR);//执行FLASH APP代码
-		//	}
-		//	else
-		//	{
-		//		printf("非FLASH应用程序,无法执行!\r\n");
-		//		LCD_ShowString(30, 210, 200, 16, 16, "Illegal FLASH APP!");
-		//	}
-		//	clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示	  
-		//}
-		//if (key == KEY0_PRES)	//KEY0按下
-		//{
-		//	printf("开始执行SRAM用户代码!!\r\n");
-		//	if (((*(vu32*)(0X20001000 + 4)) & 0xFF000000) == 0x20000000)//判断是否为0X20XXXXXX.
-		//	{
-		//		iap_load_app(0X20001000);//SRAM地址
-		//	}
-		//	else
-		//	{
-		//		printf("非SRAM应用程序,无法执行!\r\n");
-		//		LCD_ShowString(30, 210, 200, 16, 16, "Illegal SRAM APP!");
-		//	}
-		//	clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示	 
-		//}
 
-		if (key == KEY_S_PRES)	//上电后检测到 立即检测到启动开关是按下状态，开始执行固件更新
+		if (key != KEY_S_PRES && first_run_flag==1)	//上电后检测到 立即检测到启动开关是按下状态，开始执行固件更新
+		{
+			printf("开始执行FLASH用户代码!!\r\n");
+			if (((*(vu32*)(FLASH_APP1_ADDR + 4)) & 0xFF000000) == 0x08000000)//判断是否为0X08XXXXXX.
+			{
+				iap_load_app(FLASH_APP1_ADDR);//执行FLASH APP代码
+			}
+			else
+			{
+				printf("非FLASH应用程序,无法执行!\r\n");
+
+			}
+			clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示	  
+		}
+
+
+
+		
+
+		if (key == KEY_S_PRES && first_run_flag == 0)	//上电后检测到 立即检测到启动开关是按下状态，开始执行固件更新
 		{
 			if (applenth)
 			{
 				printf("开始更新固件...\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "Copying APP2FLASH...");
+
 				if (((*(vu32*)(0X20001000 + 4)) & 0xFF000000) == 0x08000000)//判断是否为0X08XXXXXX.
 				{
 					iap_write_appbin(FLASH_APP1_ADDR, USART_RX_BUF, applenth);//更新FLASH代码   
-					LCD_ShowString(30, 210, 200, 16, 16, "Copy APP Successed!!");
+
 					printf("固件更新完成!\r\n");
 				}
 				else
 				{
-					LCD_ShowString(30, 210, 200, 16, 16, "Illegal FLASH APP!  ");
+
 					printf("非FLASH应用程序!\r\n");
 				}
 			}
 			else
 			{
 				printf("没有可以更新的固件!\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "No APP!");
+
 			}
 			clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示						
 
@@ -290,10 +230,10 @@ int main(void)
 			else
 			{
 				printf("非FLASH应用程序,无法执行!\r\n");
-				LCD_ShowString(30, 210, 200, 16, 16, "Illegal FLASH APP!");
 			}
 			clearflag = 7;//标志更新了显示,并且设置7*300ms后清除显示	  
 		}
 
+		first_run_flag = 0;
 	}
 }
